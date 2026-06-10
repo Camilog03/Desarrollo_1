883b4dbbd52e40f6ed7ea1f0dae61d8c47aa7f72
Camilo Gil <giljuancamilo39@gmail.com>
2026-06-09 22:51:55 -0500

    Actualizacion resolucion de conflictos con docker

--- server-fastapi/app/database.py
+++ server-fastapi/app/database.py
@@ -1,20 +1,15 @@
-# backend/app/database.py
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Conexión directa al servicio 'db' de Docker usando las credenciales del compose
-DATABASE_URL = "postgresql://postgres:postgres@db:5432"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-# El engine es el encargado de comunicarse directamente con Postgres
+print(f"Conectando a: {DATABASE_URL}")
+
 engine = create_engine(DATABASE_URL)
-
-# Cada vez que necesites consultar la BD, usarás una instancia de SessionLocal
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Esta clase Base es de donde van a heredar tus objetos en models.py para convertirse en tablas
 Base = declarative_base()
 
-# Una función auxiliar (Dependency) que usarás en tus rutas de FastAPI para abrir/cerrar la BD
 def get_db():
     db = SessionLocal()
     try:
