7e625914468763b1639ead15b4a47ae77769f064
ManuUnivalle <manuela.delgado@correounivalle.edu.co>
2026-06-10 22:15:02 -0500

    feat: agregar routers de menu y pedidos

--- server-fastapi/app/main.py
+++ server-fastapi/app/main.py
@@ -2,10 +2,14 @@
 from fastapi import FastAPI
 from app.database import engine
 from app import models
+from app.routers import menu, pedidos
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.include_router(menu.router, prefix="/api")
+app.include_router(pedidos.router, prefix="/api")
 
 @app.get("/")
 def root():
